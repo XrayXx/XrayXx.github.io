622af4dc6edbbe7d1bd4f4d154cec3c3c22d6c19
XrayXx <46896314+XrayXx@users.noreply.github.com>
2019-01-21 18:36:28 +0300

    Update soin.js

--- soin.js
+++ soin.js
@@ -1,10 +1,12 @@
 var soinMax;
   var soinrand;
-
-    //Массив ссылок на картиноки
     soin_array = [
-    'https://pp.userapi.com/c845216/v845216667/181ba4/n6pIUE5GgsM.jpg',
-    '',
+      'https://pp.userapi.com/c845216/v845216667/181ba4/n6pIUE5GgsM.jpg',
+      'https://pp.userapi.com/c845523/v845523436/14745e/fwNI8KJaBzs.jpg',
+      'https://pp.userapi.com/c850732/v850732688/5c5ea/U5Z9tGqgnOQ.jpg',
+      'https://pp.userapi.com/c849136/v849136438/ce023/N97rncjMQok.jpg',
+      'https://pp.userapi.com/c845020/v845020868/13aeb6/To-63cYH59I.jpg',
+      'https://sun1-16.userapi.com/c543103/v543103757/54741/Q4COs4BZ1rw.jpg'
   ];
  
   soinMax = soin_array.length - 1; 
